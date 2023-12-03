266a44121ecd6022a7a727a8c4c055d5f1e14de0
Eldar Zanizdra <137602208+EldarZanizdra@users.noreply.github.com>
2023-12-03 15:41:36 +0200

    lesson

--- django_1/django_1_app/urls.py
+++ django_1/django_1_app/urls.py
@@ -1,15 +1,15 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     # path('aboutMe', views.about, name='aboutMe'),
     # path('page1', views.page1, name='page1'),
     # path('page2', views.page2, name='page2'),
     # path('page3', views.page3, name='page3'),
-    # path('filmpage', views.films, name='films'),
+    # path('films', views.films, name='films'),
     # path('book', views.books, name='books'),
-    path('register', views.registration, name='register'),
-    path('login', views.login, name='login'),
+    path('registration', views.RegistrationView.as_view(), name='register'),
+    path('login', views.LoginPage.as_view(), name='login'),
     path('project_create', views.project_create, name='project_create'),
-    path('project_page/<int:id>', views.project, name='project_page'),
+    path('project_page/<int:id>', views.ProjectView.as_view(), name='project_page'),
    ]
--- django_1/django_1_app/views.py
+++ django_1/django_1_app/views.py
@@ -1,28 +1,49 @@
 from django.shortcuts import render, redirect
-from .models import Project, Task
+from .models import Project, Task, Users
+from django.urls import reverse_lazy
 from .forms import ProjectForm, TaskForm
 from .forms import  RegistrationForm, LoginForm
 from .forms import RegistrationForm, LoginForm
 from django.contrib.auth import authenticate, login
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
 
+class ProjectView(CreateView):
+    form_class = TaskForm
+    model = Task
+    template_name = 'project_page.html'
 
-def project(request, **kwargs):
-    project = Project.objects.get(id=kwargs['id'])
-    # tasks = Task.objects.filter(project=project.id)
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            priority = form.cleaned_data['priority']
-            deadline = form.cleaned_data['deadline']
-            status = form.cleaned_data['status']
-            task = Task(name=name, priority=priority, deadline=deadline, project=project, status=status)
-            task.save()
-            return redirect(f'/')
-    else:
-        form = TaskForm()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = Project.objects.get(id=self.kwargs['id'])
         tasks = Task.objects.filter(project=project)
-        return render(request, 'project_page.html', {'project': project, 'tasks': tasks, 'form': form})
+        context['project'] = project
+        context['tasks'] = tasks
+        return context
+
+    def get_success_url(self, **kwargs):
+        return f"/project_page/{self.kwargs["id"]}"
+
+
+
+#def project(request, **kwargs):
+#    project = Project.objects.get(id=kwargs['id'])
+#    if request.method == 'POST':
+#        form = TaskForm(request.POST)
+#        if form.is_valid():
+#            name = form.cleaned_data['name']
+#            priority = form.cleaned_data['priority']
+#            deadline = form.cleaned_data['deadline']
+#            status = form.cleaned_data['status']
+#            task = Task(name=name, priority=priority, deadline=deadline, project=project, status=status)
+#           task.save()
+#            return redirect(f'/')
+#    else:
+#        form = TaskForm()
+#        tasks = Task.objects.filter(project=project)
+#        return render(request, 'project_page.html', {'project': project, 'tasks': tasks, 'form': form})
 
 
 def project_create(request):
@@ -38,44 +59,89 @@ def project_create(request):
         context = {'name': form}
         return render(request, 'project_create.html', context)
 
-def registration(request):
+
+def project_edit(request, **kwargs):
+    p = Project.objects.get(id=kwargs['id'])
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = ProjectForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password1']
-            user = User.objects.create_user(username=username, email=email, password=password)
-            user.save()
-            login(request, user)
-            return redirect('/')
+            name = form.cleaned_data['name']
+            p.name = name
+            p.save()
+            return redirect(f'/')
     else:
-        form = RegistrationForm()
-        return render(request, 'registration.html', {'form': form})
+        form = ProjectForm(initial={'name' : p.name})
+        context = {'form' : form}
+        return render(request, "project_edit.html", context)
 
 
-def login_page(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('/')
-            else:
-                return redirect('/registration')
-    else:
-        form = LoginForm()
-        return render(request, 'login.html', {'form': form})
+class RegistrationView(CreateView):
+    template_name = 'registration.html'
+    model = Users
+    form_class = RegistrationForm
+    success_url = reverse_lazy('/')
+
+    def get_success_url(self, **kwargs):
+        self.object.username
+        response = HttpResponse()
+        response.set_cookie('name', 'Bob')
+        self.request.COOKIES['name']
+        return '/'
 
 
-def home(request):
-    names = ['Eldar', 'Denis', "Maria"]
-    name = 'Geb'
-    context = {'name': name, 'title': 'Home', 'names': names}
-    return render(request, 'home.html', context)
+# def registration(request):
+#   if request.method == 'POST':
+#        form = RegistrationForm(request.POST)
+#        if form.is_valid():
+#            username = form.cleaned_data['username']
+#            email = form.cleaned_data['email']
+#            password = form.cleaned_data['password1']
+#            user = User.objects.create_user(username=username, email=email, password=password)
+#            user.save()
+#            login(request, user)
+#            return redirect('/')
+#    else:
+#        form = RegistrationForm()
+#        return render(request, 'registration.html', {'form': form})
+
+
+class LoginPage(LoginView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    redirect_authenticated_user = True
+
+
+class LogoutView(LoginView):
+    pass
+
+# def login_page(request):
+#    if request.method == 'POST':
+#        form = LoginForm(request.POST)
+#        if form.is_valid():
+#            username = form.cleaned_data['username']
+#           password = form.cleaned_data['password']
+#            user = authenticate(request, username=username, password=password)
+#            if user is not None:
+#                login(request, user)
+#                return redirect('/')
+#            else:
+#                return redirect('/registration')
+#    else:
+#        form = LoginForm()
+#        return render(request, 'login.html', {'form': form})
+
+
+class HomeView(ListView):
+    model = Project
+    template_name = 'home.html'
+    context_object_name = 'projects'
+
+
+# def home(request):
+    # user = request.user
+    # projects = Project.objects.all()
+    # context = {'username': user, 'projects': projects}
+    # return render(request, 'home.html', context)
 
 
 # def about(request):
@@ -83,7 +149,7 @@ def home(request):
 #    return render(request, 'about_me.html', context)
 
 
-#def page1(request):
+# def page1(request):
 #    if request.method == 'POST':
 #        data = request.POST
 #        name = data['name']
@@ -94,12 +160,12 @@ def home(request):
 #   return render(request, 'page1.html')
 
 
-#def page2(request):
+# def page2(request):
 #   dirs = Directors.objects.all()
 #    return render(request, 'page2.html', {'data': dirs})
 
 
-#def page3(request):
+# def page3(request):
 #    if request.method == 'POST':
 #        form = FilmForm1(request.POST)
 #        if form.is_valid():
@@ -115,6 +181,6 @@ def home(request):
 #        return render(request, 'page3.html', {'form': form})
 
 
-#def films(request):
+# def films(request):
 #    film = Films.objects.all()
 #    return render(request, 'films.html', {'films': film})
